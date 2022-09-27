6250e5133426992ba489a73ac2302a1e6ea733bd
DCL4248A <DCL4248A@u2-209-18.ens.fsi>
2022-09-27 19:55:21 +0200

    test

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,10 @@ public class Gaulois {
 	public String getNom() {
 		return nom;
 	}
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
 	public Gaulois(String nom, int force, int effetPotion) {
 		super();
 		this.nom = nom;
@@ -13,5 +17,8 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
-
+	
+	public static void main(String[] args) {
+		
+	}
 }
