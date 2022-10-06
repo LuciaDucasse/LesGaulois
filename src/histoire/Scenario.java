1e2c5db858356b425be45581d7db069402b5be0b
DCL4248A <DCL4248A@u3-205-07.ens.fsi>
2022-10-06 15:25:17 +0200

    Avancement TP2

---
+++ src/histoire/Scenario.java
@@ -0,0 +1,16 @@
+package histoire;
+
+import personnages.Gaulois;
+import personnages.Romain;
+
+public class Scenario {
+
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		asterix = prendreParole();
+		parler("Bonjour à tous")
+
+	}
+
+}
